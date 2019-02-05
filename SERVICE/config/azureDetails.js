d22aba646bf7aa8a15fe6fe03dd6b4e115d51ffa
mahesvaruna <mahes.varuna@outlook.com>
2019-02-05 13:35:09 +0800

    add config folder

---
+++ SERVICE/config/azureDetails.js
@@ -0,0 +1,5 @@
+module.exports = {
+  accessKey: "",
+  storageAccount: "",
+  container: ""
+};
---
+++ SERVICE/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  config: {
+    user: "",
+    password: "",
+    port: "",
+    server: "",
+    database: "",
+    options: {
+      encrypt: true
+    }
+  }
+};
