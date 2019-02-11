97f7470e2e4a357c3fd84160c5d6b0c52f3cb13a
mahesvaruna <mahes.varuna@outlook.com>
2019-02-11 21:38:10 +0800

    typo jsx --> js

--- SERVICE/blobController.js
+++ SERVICE/blobController.js
@@ -2,11 +2,11 @@ var mssql = require("mssql");
 var azure = require("azure-storage");
 var fs = require("fs");
 //
-var configFile = require("./config/config.jsx");
+var configFile = require("./config/config.js");
 
 var config = configFile.config;
 //Paste Azure details
-var azureDetails = require("./config/azureDetails.jsx");
+var azureDetails = require("./config/azureDetails.js");
 
 var accessKey = azureDetails.accessKey;
 var storageAccount = azureDetails.storageAccount;
