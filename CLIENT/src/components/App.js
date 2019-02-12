08e2c451753afa1149bfdd1410f89799daa15f81
Maheswaran <44431663+MahesVaruna@users.noreply.github.com>
2019-02-12 15:43:21 +0800

    react router used for nav

--- CLIENT/src/components/App.js
+++ CLIENT/src/components/App.js
@@ -4,21 +4,22 @@ import Header from "./header.js";
 import AIDetails from "./AIDetails.js";
 import ReportAndObservation from "./ReportAndObservation.js";
 import ErrorBoundary from "./HandleErrorComponent.js";
-import { history } from "../index";
+import { Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="jumbotron">
-        <button
+        <Link to="/" refresh="true">
+      <button
           className="btn btn-danger"
           style={{ position: "absolute", top: "2%", left: "90%" }}
           onClick={() => {
             localStorage.clear();
-            history.push("/");
           }}
         >
           Logout
         </button>
+       </Link>
         <h3>New Audit / Inspection Report - ATLANTIC DREAM(d)</h3>
         {/*ErrorBoundary is a React component. It catch JavaScript errors
          anywhere in their child component tree */}
